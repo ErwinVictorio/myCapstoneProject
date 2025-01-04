b0745f2afda8b7214e27d324d7efed30603e1862
Your Name <you@example.com>
2025-01-05 04:51:21 +0800

    Final Update

--- AJAX/admin_logout.php
+++ AJAX/admin_logout.php
@@ -1,1 +1,1 @@
-<?phpsession_start();session_unset();session_destroy();header('Location: ../view/Admin_login.php');exit();
+<?phpsession_start();session_unset();session_destroy();header('Location: ../index.php');exit();
